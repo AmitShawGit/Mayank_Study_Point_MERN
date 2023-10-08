22b371642114f0ed2c7b16169b9d104836c554f5
Amit <amitshaw1421@gmail.com>
2023-10-08 21:14:51 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Header from './components/header/Header'
-import Home from './pages/home/Home'
+import Home from './pages/home/Home' 
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
 const App = () => {
   return (
     <>
     <Header />
-   <Home />
+    <BrowserRouter>
+    <Routes>
+      <Route exact path='/' element={<Home />}/>
+    </Routes>
+    </BrowserRouter>
+
    </>
   )
 }
