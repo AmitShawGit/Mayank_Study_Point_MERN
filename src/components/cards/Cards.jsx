684fdac687b86f72fc8140febcc6b086775d3b25
Amit <amitshaw1421@gmail.com>
2023-10-08 15:42:25 +0530

    our services

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
 import Card from 'react-bootstrap/Card';
 import './cards.scss'
-const Cards = ({cardText , bgImg}) => {
+const Cards = ({cardText , bgImg , icon}) => {
   return (
     <>
       <Card  style={{background:`url(${bgImg})`}} className='cards'>
+    <span className="pink-overlay"></span>
       <Card.Body className='card-body'>
-        <Card.Title className='title'>{cardText}</Card.Title>
+        <Card.Title className='title'>
+          <p><i className={`${icon} icon`}></i></p>
+          <p className='text'>{cardText}</p>
+          </Card.Title>
       </Card.Body>
+    <span className="blue-overlay"></span>
     </Card>
     </>
   )
--- src/pages/home/services/Services.jsx
+++ src/pages/home/services/Services.jsx
@@ -1,58 +1,70 @@
 import React from 'react'
 import Cards from '../../../components/cards/Cards'
 import ContentWrapper from '../../../components/wrapper/ContentWrapper'
+import './style.scss';
 import { Col, Row } from 'react-bootstrap'
 import freePdf from '../../../assets/service1.png'
 import video from '../../../assets/service2.png';
 import assVid from '../../../assets/service3.png'
+import assignment from '../../../assets/service4.png'
+import  winner from '../../../assets/service5.png'
+import  free from '../../../assets/service6.png'
 const Services = () => {
     let CardDetails = [
         {
             id: 1,
             text: "Free Lecture Video",
             img: freePdf,
-            icon: "",
+            icon: "ri-youtube-line youtube-color",
             route: "/"
         },
         {
             id: 2,
-            text: "My Videpo",
+            text: "Sample Projects",
             img: video,
+            icon:"ri-projector-2-line projector",
             route: "/"
         },
         {
             id: 3,
-            text: "My Videpo",
-            img: assVid,
+            text: "Asignment Video",
+            img: assignment,
+            icon:"ri-folder-video-line assign-video",
             route: "/"
         },
         {
             id: 4,
-            text: "My Videpo",
-            img: freePdf,
+            text: "Assignment",
+            img: assVid,
+            icon:"ri-file-list-2-line service-icon",
             route: "/"
         },
         {
             id: 5,
-            text: "My Videpo",
-            img: freePdf,
+            text: "Free Pdf Notes",
+            img: free,
+            icon:"ri-file-pdf-2-line youtube-color",
             route: "/"
         },
         {
             id: 6,
-            text: "My Video",
-            img: freePdf,
+            text: "Milestones",
+            icon:"ri-medal-line projector",
+            img: winner,
             route: "/"
         },
     ]
     return (
         <>
             <ContentWrapper>
+                <h2 className='heading text-center'>Our Services</h2>
                 <Row>
                     {CardDetails.map(( item) => {
                         return (
                             <Col key={item.id} md={4}>
-                                <Cards bgImg={item.img} cardText={item.text} />
+                              
+                                <Cards bgImg={item.img} cardText={item.text} icon={item.icon}/>
+                                
                             </Col>
                         )
                     })}
