8c6df84d87cc9fa9263021b477513c153ff46785
Amit <amitshaw1421@gmail.com>
2023-10-14 21:33:40 +0530

    site design completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -20,20 +20,56 @@ const footer = () => {
         </ContentWrapper>
       </div>
       <footer>
-        <div className="footer">
+        <div className="footer pt-5">
           <ContentWrapper>
             <Row>
-              <Col md={4}>
+              <Col md={3}>
                 <ul className='footer-list'>
                   <li className='footer-head text-primary'>Quick Links</li>
                   <li className='footer-item'>Home</li>
                   <li className='footer-item'>Home</li>
                   <li className='footer-item'>Home</li>
                   <li className='footer-item'>Home</li>
-                  
+
+                </ul>
+              </Col>
+              <Col md={3}>
+                <ul className='footer-list'>
+                  <li className='footer-head text-primary'>Our Services</li>
+                  <li className='footer-item'>Home</li>
+                  <li className='footer-item'>Home</li>
+                  <li className='footer-item'>Home</li>
+                  <li className='footer-item'>Home</li>
+
+                </ul>
+              </Col>
+              <Col md={3}>
+                <ul className='footer-list'>
+                  <li className='footer-head text-primary'>Our Services</li>
+                  <li className='footer-item'>Home</li>
+                  <li className='footer-item'>Home</li>
+                  <li className='footer-item'>Home</li>
+                  <li className='footer-item'>Home</li>
+
+                </ul>
+              </Col>
+              <Col md={3} className='text-center footer-logo'>
+                <h4>Mayank's Study Point</h4>
+                <p>Your Future is in your hand</p>
+                <ul className='footer-list'>
+                  <li><i class="ri-mail-line"></i> mayank@gmail.com</li>
+                  <div className="d-flex justify-content-around mt-3">
+                    <div><i class="ri-facebook-circle-fill footer-icon-fb footer-icon"></i></div>
+                    <div><i class="ri-youtube-fill footer-icon-yt footer-icon"></i></div>
+                    <div><i class="ri-instagram-fill footer-icon-insta footer-icon"></i></div>
+                  </div>
                 </ul>
               </Col>
             </Row>
+            <div className="d-flex justify-content-between">
+<p>All right reserved by Mayank Study Point <i class="ri-copyright-line"></i> {new Date().getFullYear()}</p>
+<p>Designed & Developed by Amit Shaw</p>
+            </div>
           </ContentWrapper>
         </div>
       </footer>
