c60b900cb40c88cc2e46ec33e97d7377835574f1
Amit <amitshaw1421@gmail.com>
2023-10-14 14:36:18 +0530

    sub footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Header from './components/header/Header'
 import Home from './pages/home/Home' 
+import Footer from './components/footer/Footer'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 const App = () => {
   return (
-    <>
+  <>
     <Header />
     <BrowserRouter>
     <Routes>
       <Route exact path='/' element={<Home />}/>
     </Routes>
     </BrowserRouter>
-
+    <Footer/>
    </>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import './footer.scss'
+import ContentWrapper from '../wrapper/ContentWrapper'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 const footer = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="sub-footer mt-5">
+        <ContentWrapper>
+          <Row>
+            <Col><h3>Subscribe to Our Newsletter</h3></Col>
+            <Col>
+              <div className="email-footer-form">
+                <input type="text" className='form-control ' />
+                <button type='button' className='btn btn-primary'>Send</button>
+              </div>
+            </Col>
+          </Row>
+        </ContentWrapper>
+      </div>
+      <footer>
+        <div className="footer">
+          <ContentWrapper>
+            <Row>
+              <Col md={4}>
+                <ul className='footer-list'>
+                  <li className='footer-head text-primary'>Quick Links</li>
+                  <li className='footer-item'>Home</li>
+                  <li className='footer-item'>Home</li>
+                  <li className='footer-item'>Home</li>
+                  <li className='footer-item'>Home</li>
+                  
+                </ul>
+              </Col>
+            </Row>
+          </ContentWrapper>
+        </div>
+      </footer>
+    </>
   )
 }
 
