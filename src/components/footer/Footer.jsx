6f542a0891d8ed530fcd8245d35b76df8bd1cd6f
Amit <amitshaw1421@gmail.com>
2023-10-15 15:24:01 +0530

    changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -57,18 +57,18 @@ const footer = () => {
                 <h4>Mayank's Study Point</h4>
                 <p>Your Future is in your hand</p>
                 <ul className='footer-list'>
-                  <li><i class="ri-mail-line"></i> mayank@gmail.com</li>
+                  <li><i className="ri-mail-line"></i> mayank@gmail.com</li>
                   <div className="d-flex justify-content-around mt-3">
-                    <div><i class="ri-facebook-circle-fill footer-icon-fb footer-icon"></i></div>
-                    <div><i class="ri-youtube-fill footer-icon-yt footer-icon"></i></div>
-                    <div><i class="ri-instagram-fill footer-icon-insta footer-icon"></i></div>
+                    <div><i className="ri-facebook-circle-fill footer-icon-fb footer-icon"></i></div>
+                    <div><i className="ri-youtube-fill footer-icon-yt footer-icon"></i></div>
+                    <div><i className="ri-instagram-fill footer-icon-insta footer-icon"></i></div>
                   </div>
                 </ul>
               </Col>
             </Row>
             <hr style={{color:"silver"}}/>
             <div className="d-flex justify-content-between">
-<p>All right reserved by Mayank Study Point <i class="ri-copyright-line"></i> {new Date().getFullYear()}</p>
+<p>All right reserved by Mayank Study Point <i className="ri-copyright-line"></i> {new Date().getFullYear()}</p>
 <p>Designed & Developed by Amit Shaw</p>
             </div>
           </ContentWrapper>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,18 +7,19 @@ import { useState } from 'react';
 const Header = () => {
   let [menu, setMenu] = useState(false)
   let [scrollY, setScrollY] = useState(false)
+
   return (
     <>
       <Navbar className='navbar '>
         <Container>
           <Navbar.Brand href="#home">Mayank's Study Point</Navbar.Brand>
-          <Nav className={`responsive ${menu ? "" : "hide"}`}>
+          <Nav className={`responsive ${menu ? "show" : "hide"} `}>
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
           <div className="toggle-button">
-            <i class="ri-menu-3-fill" onClick={() => setMenu(prev => !prev)}></i>
+            <i className="ri-menu-3-fill" onClick={() => setMenu(prev => !prev)}></i>
           </div>
         </Container>
       </Navbar>
