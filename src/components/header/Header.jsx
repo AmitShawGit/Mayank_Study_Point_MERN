4be31f63e823f1d107e7fe81941d90386705914a
Amit <amitshaw1421@gmail.com>
2023-10-18 21:40:31 +0530

    changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,20 @@
+import React, { useState} from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './header.scss'
-import { useState } from 'react';
+
 
 const Header = () => {
-  let [menu, setMenu] = useState(false)
-  let [scrollY, setScrollY] = useState(false)
+  let [menu, setMenu] = useState(true);
 
+ 
   return (
     <>
-      <Navbar className='navbar '>
+      <Navbar className='navbar'>
         <Container>
           <Navbar.Brand href="#home">Mayank's Study Point</Navbar.Brand>
-          <Nav className={`responsive ${menu ? "show" : "hide"} `}>
+          <Nav className={`responsive ${menu ? "show" : "hide"}`} >
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
