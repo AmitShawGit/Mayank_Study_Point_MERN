890aef66a382ad64bba227b578bea85639dd92c2
Amit <amitshaw1421@gmail.com>
2023-10-19 21:42:27 +0530

    menu responsive done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,32 +1,29 @@
-import React, { useState} from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React,{useState} from 'react';
 import './header.scss'
-
-
-const Header = () => {
-  let [menu, setMenu] = useState(true);
-
- 
-  return (
+import ContentWrapper from '../wrapper/ContentWrapper';
+let Header = ()=>{
+let [menu,setMenu]=useState(false);
+  return(
     <>
-      <Navbar className='navbar'>
-        <Container>
-          <Navbar.Brand href="#home">Mayank's Study Point</Navbar.Brand>
-          <Nav className={`responsive ${menu ? "show" : "hide"}`} >
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-          <div className="toggle-button">
-            <i className="ri-menu-3-fill" onClick={() => setMenu(prev => !prev)}></i>
-          </div>
-        </Container>
-      </Navbar>
-
+<header>
+  <nav className='navbar space-around align-center'>
+    <ContentWrapper>
+    <div className="logo-section">
+      <h4 className='mb-0'>Mayank's Study Point</h4>
+    </div>
+    <div className="menu">
+      <ul className={`nav-menu ${menu ? "show" : ""}`}>
+        <li className='nav-items'>Home</li>
+        <li className='nav-items'>Home</li>
+        <li className='nav-items'>Home</li>
+        <li className='nav-items'>Home</li>
+      </ul>
+      <i class="ri-menu-3-line bar-btn" onClick={()=> {setMenu(prev => !prev)}}></i>
+    </div>
+    </ContentWrapper>
+  </nav>
+  </header>    
     </>
-  );
+  )
 }
-
 export default Header;
