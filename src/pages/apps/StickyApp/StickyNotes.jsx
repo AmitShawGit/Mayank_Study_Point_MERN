464cff0394dadbc23cb1e200505a25eb575a0ba7
Amit <amitshaw1421@gmail.com>
2023-11-13 21:23:14 +0530

    sample form data taking on console

--- src/pages/apps/StickyApp/StickyNotes.jsx
+++ src/pages/apps/StickyApp/StickyNotes.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { useState } from 'react';
 
 const StickyNotes = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    let [note, setNote] = useState({ title: " " });
+    let handleChange = (e) => {
+
+        setNote({ ...note, [e.target.name]: e.target.value })
+    };
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(note);
+        localStorage.setItem("title", note.title)
+    } 
+    let previewNote = localStorage.getItem("title")
+    return (
+        <div>
+            <form action="" onSubmit={handleSubmit}>
+                <input type="text" placeholder='Heading' name='title' value={note.title} onChange={handleChange} />
+                <button>Save</button>
+            </form>
+
+            <p>{previewNote}</p>
+        </div>
+    )
 }
 
 export default StickyNotes
