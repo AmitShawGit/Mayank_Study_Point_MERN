59ae769237c9c6bdf96b4c7b2a726de532076968
Amit <amitshaw1421@gmail.com>
2023-12-02 17:59:57 +0530

    started adding links

--- src/pages/home/hero/Hero.jsx
+++ src/pages/home/hero/Hero.jsx
@@ -5,7 +5,7 @@ import Img from '../../../components/lazyloading/LazyLoading'
 import menHero from '../../../assets/manhero1.png'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 const Hero = () => {
   return (
     <>
@@ -15,7 +15,7 @@ const Hero = () => {
             <Col md={6}>
               <h2>We are with students from starting to end</h2>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus quam, sit repellendus dicta, nemo ex et sed quas officia, rem explicabo natus exercitationem ipsum provident Maiores corrupti et pariatur animi</p>
-            <Button className='btn btn-primary'>Contact Us</Button>
+            <Link to="/contact" className='btn btn-primary'>Contact Us</Link>
             </Col>
             <Col md={6} className='right-section'>
               
--- src/pages/home/services/Services.jsx
+++ src/pages/home/services/Services.jsx
@@ -8,7 +8,8 @@ import video from '../../../assets/service2.png';
 import assVid from '../../../assets/service3.png'
 import assignment from '../../../assets/service4.png'
 import winner from '../../../assets/service5.png'
-import free from '../../../assets/service6.png'
+import free from '../../../assets/service6.png';
+import { Link } from 'react-router-dom';
 const Services = () => {
     let CardDetails = [
         {
@@ -23,7 +24,7 @@ const Services = () => {
             text: "Sample Projects",
             img: video,
             icon: "ri-projector-2-line projector",
-            route: "/"
+            route: "https://drive.google.com/drive/folders/1n7sIddEM6A2u2UcVfU_GlE1UEm9unrQx"
         },
         {
             id: 3,
@@ -63,7 +64,7 @@ const Services = () => {
                         return (
                             <Col key={item.id} md={4} >
 
-                                <Cards bgImg={item.img} cardText={item.text} icon={item.icon}  />
+                                <Link to={item.route} target='_blank'> <Cards bgImg={item.img} cardText={item.text} icon={item.icon} /> </Link>
 
                             </Col>
                         )
