decdfdff863e8a9144114490d17c2932d33b5c71
amit <amitshaw1421@gmail.com>
2023-12-14 12:07:50 +0530

    images

--- src/pages/apps/Apps.jsx
+++ src/pages/apps/Apps.jsx
@@ -5,16 +5,18 @@ import Cards from '../../components/cards/Cards'
 
 import ContentWrapper from '../../components/wrapper/ContentWrapper';
 import { Link } from 'react-router-dom';
+import notesapp from '../../assets/notesapp.png'
+import agecalc from '../../assets/agecalc.png'
 const Apps = () => {
   return (
     <>
     <ContentWrapper>
       <Row>
         <Col sm={12} md={4}>
-        <Link to="/notes"><Cards cardText="Sticky Notes App" /></Link>
+        <Link to="/notes"><Cards cardText="Sticky Notes App" bgImg={notesapp}/></Link>
         </Col>
         <Col sm={12} md={4}>
-        <Link to="/age-calculator"><Cards cardText="Age Calulator App" /></Link>
+        <Link to="/age-calculator"><Cards cardText="Age Calulator App" bgImg={agecalc} /></Link>
         </Col>
         </Row>
       
