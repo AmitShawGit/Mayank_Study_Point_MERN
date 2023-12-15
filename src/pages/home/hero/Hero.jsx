95561fd6d9abf43bc1db6f2e74f680540c0d4c46
amit <amitshaw1421@gmail.com>
2023-12-15 16:33:05 +0530

    changes

--- src/pages/home/hero/Hero.jsx
+++ src/pages/home/hero/Hero.jsx
@@ -13,9 +13,8 @@ const Hero = () => {
         <ContentWrapper>
           <Row className='hero'>
             <Col md={6}>
-              <h2>We are with students from starting to end</h2>
-            <p>Hello visitors, this is my little effort to help the <strong>#NMIMS</strong> MBA students. Here I have shares the references to help you guys get a pathway of the answers for the NMIMS assignment. I am sure my visitors will benefit by watching videos and contacting us !
-            </p>
+              <h2>"We are with students from starting to end."</h2>
+            <p>Embark on a seamless journey to academic success with our dedicated platform. This space is crafted to empower students like you in conquering assignments with confidence. Discover a wealth of resources and references curated to guide you through your academic quests</p>
             <Link to="/contact" className='btn btn-primary'>Contact Us</Link>
             </Col>
             <Col md={6} className='right-section'>
