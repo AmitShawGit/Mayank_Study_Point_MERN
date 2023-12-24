b2f3cda370cea1a0b7261e3fc536ce37eeed0da1
amit <amitshaw1421@gmail.com>
2023-12-24 21:15:09 +0530

    select function and texthidden option completed

--- src/pages/services/freepdf/Freepdf.jsx
+++ src/pages/services/freepdf/Freepdf.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ContentWrapper from "../../../components/wrapper/ContentWrapper";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -6,43 +6,141 @@ import Card from 'react-bootstrap/Card'
 import { Link } from "react-router-dom";
 
 let Freepdf = () => {
-
+let [subject,setSubject]=useState([])
     let select = [
         {
             id: 1,
             name: "NMIMS",
+            subject: [
+                {
+                    id: 1,
+                    subjectName: "NMIMS Studies",
+                    semester: "1",
+                    detail: " Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    path: "/home",
+                    cart: "/cart"
+                },
+                {
+                    id: 2,
+                    subjectName: "Accountancy",
+                    semester: "1",
+                    detail: " Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content",
+                    path: "/home",
+                    cart: "/cart"
+                },
+                {
+                    id: 2,
+                    subjectName: "Accountancy",
+                    semester: "1",
+                    detail: " Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content",
+                    path: "/home",
+                    cart: "/cart"
+                },
+                {
+                    id: 2,
+                    subjectName: "Accountancy",
+                    semester: "1",
+                    detail: " Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content",
+                    path: "/home",
+                    cart: "/cart"
+                },
+                {
+                    id: 2,
+                    subjectName: "Accountancy",
+                    semester: "1",
+                    detail: " Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content",
+                    path: "/home",
+                    cart: "/cart"
+                },
+                {
+                    id: 2,
+                    subjectName: "Accountancy",
+                    semester: "1",
+                    detail: " Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content",
+                    path: "/home",
+                    cart: "/cart"
+                },
+                {
+                    id: 2,
+                    subjectName: "Accountancy",
+                    semester: "1",
+                    detail: " Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content",
+                    path: "/home",
+                    cart: "/cart"
+                },
+                {
+                    id: 2,
+                    subjectName: "Accountancy",
+                    semester: "1",
+                    detail: " Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content",
+                    path: "/home",
+                    cart: "/cart"
+                },
+                {
+                    id: 2,
+                    subjectName: "Accountancy",
+                    semester: "1",
+                    detail: " Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content",
+                    path: "/home",
+                    cart: "/cart"
+                },
+            ]
         },
         {
             id: 2,
             name: "Amity University",
+            subject: [
+                {
+                    id: 1,
+                    subjectName: "Amity Studies",
+                    semester: "1",
+                    detail: " Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    path: "/home",
+                    cart: "/cart"
+                },
+                {
+                    id: 2,
+                    subjectName: "Accountancy",
+                    semester: "1",
+                    detail: " Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content",
+                    path: "/home",
+                    cart: "/cart"
+                },
+            ]
         },
         {
             id: 3,
             name: "Burdwan University",
+            subject: [
+                {
+                    id: 1,
+                    subjectName: "Bussiness Studies",
+                    semester: "1",
+                    detail: " Some quick example text to build on the card title and make up the bulk of the card's content.",
+                    path: "/home",
+                    cart: "/cart"
+                },
+                {
+                    id: 2,
+                    subjectName: "Accountancy",
+                    semester: "1",
+                    detail: " Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content",
+                    path: "/home",
+                    cart: "/cart"
+                },
+            ]
         },
-    ]
 
-    let subject = [
-        {
-            id:1,
-            subjectName:"Bussiness Studies",
-            semester: "1",
-            detail:" Some quick example text to build on the card title and make up the bulk of the card's content.",
-            path:"/home",
-            cart:"/cart"
-        },
-        {
-            id:2,
-            subjectName:"Accountancy",
-            semester: "1",
-            detail:" Some quick example text to build on the card title and make up the bulk of the card's content.Some quick example text to build on the card title and make up the bulk of the card's content",
-            path:"/home",
-            cart:"/cart"
-        },
     ]
+    //Select University
+    const getSelectedVal = (event) => {
+        let selected = event.target.value;
+       let find = select.find((item)=>{return item.name===selected})
+       setSubject(find ? find.subject : []);
+    }
+
     return (
         <>
-
             <ContentWrapper>
                 <div className="select-university">
 
@@ -51,9 +149,10 @@ let Freepdf = () => {
                             <label htmlFor="select">Select University</label>
                         </Col>
                         <Col md={8}>
-                            <select name="" id="select" className="form-control">
-                                {select.map((value,index) => {
-                                    return(
+                            <select id="select" className="form-control" onChange={getSelectedVal}>
+                                <option value="Select">Select University</option>
+                                {select.map((value, index) => {
+                                    return (
                                         <option key={index} value={value.name}>{value.name}</option>
                                     )
                                 })}
@@ -64,29 +163,28 @@ let Freepdf = () => {
                 </div>
 
                 <Row>
-                    {subject.map((value,id)=>{
-                        return(
-                            <Col md={3} key={id}>
-                            <Card className="subject-card">
-                                <Card.Body>
-                                    <Card.Title>{value.subjectName}</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted">Semester {value.semester}</Card.Subtitle>
-                                    <Card.Text>
-                                       {value.detail}
-                                    </Card.Text>
-                                    <Row>
-                                        <Col>  <Link to={value.cart}>Add to Cart</Link></Col>
-                                        <Col>
-                                        <Link to={value.path}>Buy Now</Link></Col>
-                                    </Row>
-                                  
-                                    
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        )
-                    })}
-                
+                    {    subject.map((subject) => (
+                            <Col md={3} key={subject.id}>
+                                <Card className="subject-card">
+                                    <Card.Body>
+                                        <Card.Title>{subject.subjectName}</Card.Title>
+                                        <Card.Subtitle className="mb-2 text-muted">Semester {subject.semester}</Card.Subtitle>
+                                        <Card.Text>{subject.detail}</Card.Text>
+                                        <Row>
+                                            <Col>
+                                                <Link to={subject.cart}>Add to Cart</Link>
+                                            </Col>
+                                            <Col>
+                                                <Link to={subject.path}>Buy Now</Link>
+                                            </Col>
+                                        </Row>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))
+}
+
+
                 </Row>
             </ContentWrapper>
         </>
