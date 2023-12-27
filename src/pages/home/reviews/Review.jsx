0aeb44bd3259510b8209a2951bd473dcde03a774
amit <amitshaw1421@gmail.com>
2023-12-27 18:06:17 +0530

    space formated

--- src/pages/home/reviews/Review.jsx
+++ src/pages/home/reviews/Review.jsx
@@ -11,51 +11,51 @@ import 'swiper/css';
 const Review = () => {
     return (
         <>
-      
-       
-     
+
+
+
             <ContentWrapper>
                 <h2 className='heading'>Student's Review</h2>
                 <Swiper
-                autoplay={true}
-        slidesPerView={'auto'}
-        spaceBetween={30}
-        className="mySwiper"
-      >
-                <SwiperSlide>
-                <Row className='d-flex align-items-center'>
-                    <Col md={4}>
-                        <Img src={janta} className="img-fluid" />
-                    </Col>
-                    <Col md={8} className='review'>
-                        <p className='text-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cum aut, perferendis illum odio fugit sint sequi facere repellendus quos.</p>
-                    <p className='text-primary text-20'>- Amit Shaw</p>
-                    </Col>
-                </Row>
-                </SwiperSlide>
-                <SwiperSlide>
-                <Row className='d-flex align-items-center'>
-                    <Col md={4}>
-                        <Img src={janta} className="img-fluid" />
-                    </Col>
-                    <Col md={8} className='review'>
-                        <p className='text-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cum aut, perferendis illum odio fugit sint sequi facere repellendus quos.</p>
-                    <p className='text-primary text-20'>- Amit Shaw</p>
-                    </Col>
-                </Row>
-                </SwiperSlide>
-                <SwiperSlide>
-                <Row className='d-flex align-items-center'>
-                    <Col md={4}>
-                        <Img src={janta} className="img-fluid" />
-                    </Col>
-                    <Col md={8} className='review'>
-                        <p className='text-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cum aut, perferendis illum odio fugit sint sequi facere repellendus quos.</p>
-                    <p className='text-primary text-20'>- Amit Shaw</p>
-                    </Col>
-                </Row>
-                </SwiperSlide>
-            </Swiper>
+                    autoplay={true}
+                    slidesPerView={'auto'}
+                    spaceBetween={30}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <Row className='d-flex align-items-center'>
+                            <Col md={4}>
+                                <Img src={janta} className="img-fluid" />
+                            </Col>
+                            <Col md={8} className='review'>
+                                <p className='text-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cum aut, perferendis illum odio fugit sint sequi facere repellendus quos.</p>
+                                <p className='text-primary text-20'>- Amit Shaw</p>
+                            </Col>
+                        </Row>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Row className='d-flex align-items-center'>
+                            <Col md={4}>
+                                <Img src={janta} className="img-fluid" />
+                            </Col>
+                            <Col md={8} className='review'>
+                                <p className='text-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cum aut, perferendis illum odio fugit sint sequi facere repellendus quos.</p>
+                                <p className='text-primary text-20'>- Amit Shaw</p>
+                            </Col>
+                        </Row>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Row className='d-flex align-items-center'>
+                            <Col md={4}>
+                                <Img src={janta} className="img-fluid" />
+                            </Col>
+                            <Col md={8} className='review'>
+                                <p className='text-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et cum aut, perferendis illum odio fugit sint sequi facere repellendus quos.</p>
+                                <p className='text-primary text-20'>- Amit Shaw</p>
+                            </Col>
+                        </Row>
+                    </SwiperSlide>
+                </Swiper>
             </ContentWrapper>
         </>
     )
