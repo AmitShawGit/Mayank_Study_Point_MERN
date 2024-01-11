87b83e3ffa13e673419db1c99a6b067d8d94aa0e
amit <amitshaw1421@gmail.com>
2024-01-11 20:48:33 +0530

    phonepay design and card

--- src/payment/Phonepay.jsx
+++ src/payment/Phonepay.jsx
@@ -1,4 +1,4 @@
-import React, { createRef } from 'react'
+import React, { createRef, useState } from 'react'
 import Input from '../components/formelement/Input.jsx'
 import { useFormik } from 'formik';
 import apiCall from '../services/index.ts';
@@ -13,11 +13,13 @@ let initialValues = {
     email: "",
     contact: "",
     amount: "",
+    screenshot: ""
 }
 const Phonepay = () => {
-const checked = createRef();
-
-
+    const checked = createRef();
+    let [disable, setDisable] = useState(true)
+    let [showBarcode, setShowBarcode] = useState(false)
+   
     const { values, errors, handleBlur, handleChange, handleSubmit } = useFormik({
         initialValues: initialValues,
         onSubmit: (values, action) => {
@@ -69,11 +71,23 @@ const checked = createRef();
             type: "number",
             name: "amount"
         },
+        {
+            id: 5,
+            label: "Paymant Screenshot",
+            type: "file",
+            name: "paymant-screnshot"
+        },
     ]
-    const checkBox = ()=>{
-        if(checked.current.checked){
-
+    const checkBox = () => {
+        if (checked.current.checked) {
+            setDisable(false)
+        } else if (checked.current.checked !== true) {
+            setDisable(true)
         }
+    }
+    let payBtnClicked = () => {
+        setShowBarcode(!showBarcode)
+       
     }
     return (
         <>
@@ -83,16 +97,16 @@ const checked = createRef();
                         <Col sm={6}>
                             <div className="payment-terms">
                                 <h3>Payment Terms</h3>
-                                <ul className='list-none'>
-                                    <li>(a) Payment form must be filled first to get the barcode</li>
-                                    <li>(b) After Payment one need to take the screenshot of paid amount and share the screen shot on mail</li>
+                                <ul className='list-none p-2' style={{ display: showBarcode ? "none" : "block" }}>
+                                    <li>(a) Payment form must be filled first to get the payment confirmation mail</li>
+                                    <li>(b) After Payment one need to take the screenshot of paid amount and share the screen shot on mail as well as attach with form</li>
                                     <li>(c) The verification of payment takes time upto two days</li>
                                     <li>(d) Once you verified you will get the futher proceses</li>
-                                    <li><input type="checkbox" ref={checked} onClick={checkBox}/>I accept the following terms and conditions</li>
+                                    <li><input type="checkbox" ref={checked} onClick={checkBox} id="checkbox" /> &nbsp;<label htmlFor="checkbox"> I accept the following terms and conditions</label></li>
                                 </ul>
-                                <button className='btn btn-success mb-2' >Ready to Pay</button>
+                                <button className='btn btn-success mb-2' disabled={disable} onClick={payBtnClicked} style={{ display: showBarcode ? "none" : "block" }} >Show Barcode</button>
                             </div>
-                            <img src={Barcode} alt="" className='img-fluid' />
+                            <img src={Barcode} alt="" className='img-fluid barcode-img' style={{ display: showBarcode ? "block" : "none" }} />
                         </Col>
                         <Col sm={6}>
                             <form action='/submit' onSubmit={handleSubmit}>
