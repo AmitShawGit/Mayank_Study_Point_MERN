6eab580fe72732aba09503505d39ce198663f9d2
amit <amitshaw1421@gmail.com>
2024-01-16 15:44:22 +0530

    imagewith api url getting still img is nor visible

--- src/pages/services/freepdf/VIewProd.jsx
+++ src/pages/services/freepdf/VIewProd.jsx
@@ -1,12 +1,13 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductView from '../../../components/product/ProductView';
 import ContentWrapper from '../../../components/wrapper/ContentWrapper';
 import { useNavigate, useParams } from 'react-router-dom';
 import apiCall from '../../../services/index.ts';
 const VIewProd = () => {
     let navigate = useNavigate()
-    let id =useParams()
-let [product , setProduct] = useState({id:""})
+    let id = useParams()
+    let [product, setProduct] = useState([])
+    let imageURL = process.env.REACT_APP_BASE_URL
     //go direct to  payment page
     const goToPayment = (id) => {
         console.log(id);
@@ -20,8 +21,7 @@ let [product , setProduct] = useState({id:""})
             await apiCall.get(`/view-specific-assignment/${intId}`)
                 .then((res) => {
                     let dataReturned = res.data
-                    console.log(dataReturned[0].id);
-                    setProduct({id:dataReturned[0].id})
+                    setProduct(dataReturned)
                 })
                 .catch((err) => err)
         }
@@ -32,13 +32,12 @@ let [product , setProduct] = useState({id:""})
     //api call
     useEffect(() => {
         getUniversityList()
-        console.log(product);
     }, [])
     return (
         <ContentWrapper >
             <div className="product-view-page">
 
-            <ProductView img="https://blogassets.leverageedu.com/blog/wp-content/uploads/2019/11/23171842/Business-Studies.png" title="Product Title" short_description="Short Description Lorem ipsum dolor sit amet." description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium tenetur amet veniam maxime, odio unde rerum et voluptatem eius atque? Nesciunt, voluptates. Iste reiciendis doloremque nihil dolores sequi aspernatur dolore perspiciatis dolor. Minus sed voluptatem corrupti veniam impedit sunt eos quidem dignissimos delectus expedita, voluptates sapiente omnis odio assumenda nostrum!" buyNow={()=>goToPayment(id)} />
+                <ProductView img={imageURL + product.image} title={product.subject_name} short_description={product.short_description} description={product.description} buyNow={() => goToPayment(id)} />
             </div>
         </ContentWrapper>
     )
