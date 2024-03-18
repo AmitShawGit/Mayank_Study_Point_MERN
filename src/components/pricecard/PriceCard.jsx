f9db4d31f6de5c2bf0a71cc5c08fa50cacad7113
amit <amitshaw1421@gmail.com>
2024-03-18 18:21:12 +0530

    navigate removed

--- src/components/pricecard/PriceCard.jsx
+++ src/components/pricecard/PriceCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import './style.scss';
-import { Navigate } from 'react-router-dom';
+
 const PriceCard = ({ head, features, path, goToPay }) => {
   let feature = features
   let navigate = useNavigate()
