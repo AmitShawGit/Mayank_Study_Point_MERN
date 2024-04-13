782d3e9ab46fd57b2f0afdd5c3bbe278c200051b
Amit <amitshaw1421@gmail.com>
2024-04-13 20:49:57 +0530

    hero formatted

--- src/pages/home/hero/Hero.jsx
+++ src/pages/home/hero/Hero.jsx
@@ -13,16 +13,16 @@ const Hero = () => {
           <Row className='hero'>
             <Col md={6}>
               <h1>"We are with students from starting to end."</h1>
-            <p>Embark on a seamless journey to academic success with our dedicated platform. This space is crafted to empower students like you in conquering assignments with confidence. Discover a wealth of resources and references curated to guide you through your academic quests</p>
-            <Link to="/contact" className='btn btn-primary'>Contact Us</Link>
+              <p>Embark on a seamless journey to academic success with our dedicated platform. This space is crafted to empower students like you in conquering assignments with confidence. Discover a wealth of resources and references curated to guide you through your academic quests</p>
+              <Link to="/contact" className='btn btn-primary'>Contact Us</Link>
             </Col>
             <Col md={6} className='right-section'>
-              
-                <div className="eclipse"></div>
-                <div className="men-img">
-                  <Img src={menHero} />
-                </div>
-          
+
+              <div className="eclipse"></div>
+              <div className="men-img">
+                <Img src={menHero} />
+              </div>
+
             </Col>
           </Row>
         </ContentWrapper>
