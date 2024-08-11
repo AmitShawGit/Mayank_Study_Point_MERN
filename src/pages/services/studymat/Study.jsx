fa961805a8d9a68d9dfb6f513596f59a97ff5b12
amit <amitshaw1421@gmail.com>
2024-08-11 10:58:06 +0530

    removed dependency

--- src/pages/services/studymat/Study.jsx
+++ src/pages/services/studymat/Study.jsx
@@ -80,11 +80,11 @@ let Assignment = () => {
         navigate(`/view-free-pdf/${id}`)
     }
 
-    //go direct to  payment page
-    const goToPayment = (id) => {
-        console.log(id);
-        navigate(`/Phonepay/${id}`)
-    }
+    // //go direct to  payment page
+    // const goToPayment = (id) => {
+    //     console.log(id);
+    //     navigate(`/Phonepay/${id}`)
+    // }
 
     //get university list
     const getUniversityList = async () => {
